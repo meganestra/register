ba1e68ad696e49c441d3e2d76941c781dff1c665
Megan Strachan <m.e.strachan@gmail.com>
2016-06-09 14:10:56 +0100

    initial commit

---
+++ class_register.rb
@@ -0,0 +1,63 @@
+# build nested hash array loop
+# no blocks
+
+# gets user input
+
+# class register
+# all present?
+# homework complete?
+
+# tests
+
+# count = 0
+# while (count < lines.length)
+#   puts lines[count]
+#   count += 1
+# end
+
+
+#ask for a student name
+#user inputs student name
+#student name is stored
+#ask user for next student name
+
+register = []
+
+puts "Please confirm how many students are in the class: "
+
+number_of_students = gets.chomp.to_i
+
+count = 0
+limit = number_of_students
+
+while (count < number_of_students)
+
+  def 
+  
+
+  puts "Please enter the student's first name: "
+  first_name = gets.chomp
+
+  puts "Please enter the student's last name: "
+  last_name = gets.chomp
+
+  full_name = "#{first_name} #{last_name}"
+  register << full_name
+
+  count += 1
+
+end
+
+puts register
+
+
+
+
+
+
+
+
+
+
+
+
